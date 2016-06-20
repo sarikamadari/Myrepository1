ef6c047a64ab6c5962ceefa6e873aea36fb50d55
sarikamadari <sarika.madari@tcs.com>
2016-06-20 07:17:35 -0400

    add subtract output

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -14,6 +14,8 @@ namespace ConsoleApplication2
             Calculator cal=new Calculator();
             var result = cal.sum(2,4);
             Console.WriteLine(result);
+            var result1 = cal.subtract(2, 4);
+            Console.WriteLine(result1);
             Console.ReadLine();
         }
     }
