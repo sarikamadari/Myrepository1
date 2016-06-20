d8690e3dfbbfe78936b89756f7091d0c354c4f69
sarikamadari <sarika.madari@tcs.com>
2016-06-20 05:40:02 -0400

    calculator lib.cs was added

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calculatorlib;
 
 namespace ConsoleApplication2
 {
@@ -10,6 +11,10 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
+            Calculator cal=new Calculator();
+            var result = cal.sum(2,4);
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
